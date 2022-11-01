d9dca5fe98f290f8ec48e72ff151ba428da54317
Yahya-Idris-A <yahya.idris.abdurrahman@mail.ugm.ac.id>
2022-11-01 09:33:49 +0700

    pertemuan_9

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Models\Post;
+use App\Mail\SendEmail;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\SendEmailController;
 use App\Http\Controllers\Auth\LoginController;
 
 /*
@@ -29,24 +32,36 @@ Route::get('/home', function () {
 
 Route::get('/projects', [PostController::class, 'index_projects']);
 
-Route::resource('posts','App\Http\Controllers\PostController');
+Route::resource('posts', 'App\Http\Controllers\PostController');
 
 Auth::routes([
-    'reset' => false,
+    'reset' => true,
 ]);
 
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::middleware(['auth'])->group(function(){
+Route::middleware(['auth'])->group(function () {
     Route::get('/about', function () {
         return view('about', [
             "title" => "About"
         ]);
     });
-    
+
     Route::get('/education', function () {
         return view('education', [
             "title" => "Education"
         ]);
     });
 });
+
+// Route::get('/send-email', function () {
+//     $data = [
+//         'name' => 'Nama Anda',
+//         'body' => 'Testing Kirim Email'
+//     ];
+//     Mail::to('yahyaidrisabdurrahmanz@gmail.com')->send(new SendEmail($data));
+//     dd("Email Berhasil dikirim.");
+// });
+
+Route::get('/send-email', [SendEmailController::class, 'index'])->name('kirim-email');
+Route::post('/post-email', [SendEmailController::class, 'store'])->name('post-email');
